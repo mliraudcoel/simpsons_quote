67373baad443a1f2e5f6c0d6f5680c4ba2454563
Mélanie Liraud-Coël <melanie.liraud@gmail.com>
2019-10-08 15:59:33 +0200

    Création de la navbar

---
+++ src/Components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './Navbar.css';
+
+const Navbar = () => {
+    return (
+    <nav className = 'Navbar'><a href ='https://fr.wikipedia.org/wiki/Les_Simpson'>Les Simpsons</a></nav>
+    )
+    
+}
+
+export default Navbar;
