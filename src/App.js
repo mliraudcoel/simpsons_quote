875e7f888bc0d559ba64969eb9a7ac8026fe5d40
Mélanie Liraud-Coël <melanie.liraud@gmail.com>
2019-10-08 16:10:58 +0200

    Ajout de la QuoteCard

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import Navbar from './Components/Navbar';
+import QuoteCard from './Components/QuoteCard';
 
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <QuoteCard />
     </div>
   );
 }
